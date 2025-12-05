e03f5d154fb0a4b6891a1b9c9133dd4476f341c3
Lior <liormedan1@gmail.com>
2025-12-05 21:09:07 +0200

    Update dependencies: fix security vulnerability and update Sanity packages

--- lib/sanity.image.ts
+++ lib/sanity.image.ts
@@ -1,9 +1,9 @@
-﻿import imageUrlBuilder from "@sanity/image-url"
+﻿import {createImageUrlBuilder} from "@sanity/image-url"
 import type {ImageUrlBuilder} from "@sanity/image-url/lib/types/builder"
 import type {SanityImageSource} from "@sanity/image-url/lib/types/types"
 
 import {dataset, projectId} from "./env"
 
-const builder: ImageUrlBuilder = imageUrlBuilder({projectId, dataset})
+const builder: ImageUrlBuilder = createImageUrlBuilder({projectId, dataset})
 
 export const urlFor = (source: SanityImageSource) => builder.image(source)
